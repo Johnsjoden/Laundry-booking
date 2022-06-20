ddec37fedd7b9266156caf86205d90411d10be9e
john sjödén <johnsjoden95@gmail.com>
2022-06-20 20:12:35 +0200

    delete laundry and remove objectId of laundry from Users laundry array.

--- server/routes/laundry.js
+++ server/routes/laundry.js
@@ -1,18 +1,22 @@
 const express = require("express")
 const {verify} = require("../controllers/auth")
-const {getAllLaundries, bookLaundry, createLaundry} = require("../controllers/laundry")
+const {getAllLaundries, bookLaundry, createLaundry, unbookLaundry} = require("../controllers/laundry")
 const laundry = express.Router()
 
 laundry.get("/create", verify, async (req, res, next) => {
     const result = await createLaundry(req.query)
     res.json({result})
 })
-laundry.post("/:id", verify, async (req, res, next) => {
-    const result = await bookLaundry(req.params.id, req.user.userId)
+laundry.post("/", verify, async (req, res, next) => {
+    const result = await bookLaundry(req.body, req.user.userId)
     res.json({result})
 })
 laundry.get("/", verify, async (req, res, next) => {
     const result = await getAllLaundries(req.query)
+    res.json({result})
+})
+laundry.delete("/:id", verify, async (req, res, next) => {
+    const result = await unbookLaundry(req, res, next)
     res.json({result})
 })
 module.exports = {laundry}
