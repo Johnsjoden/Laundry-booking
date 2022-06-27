514ca56879f6935b1662770a263262a22c7da6b9
johnsjoden <johnsjoden95@gmail.com>
2022-06-27 12:50:40 +0200

    logout and navigate. token as hidden on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,48 @@
 import { AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
 import { createContext } from "react";
-import { Route, Routes , Link} from "react-router-dom";
-import Home from "./pages/Home";
+import { Route, Routes , Link, Navigate, useNavigate} from "react-router-dom";
+import Home from "./pages/Book";
 import Login from "./pages/Login";
 import Register from "./pages/Register"
 import User from "./pages/User";
 const myContext = createContext()
 function App() {
+  const navigate = useNavigate()
   const token = localStorage.getItem("key")
   const config = {
     headers: {
       "Authorization": "Bearer " + token
     }
   }
+  const handleLogOut = () => {
+    localStorage.clear()
+    navigate("/login")
+    window.location.reload()
+  }
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar >
-          <Button component={Link} to="/home" variant="contained" color="primary">
-          Home
-          </Button>
-          <Button component={Link} to="/register" variant="contained" color="primary">
+          {token ? <Button component={Link} to="/book" variant="contained" color="primary">
+          Book
+          </Button> : ""}
+          {token ? "" :<Button component={Link} to="/register" variant="contained" color="primary">
           Register
-          </Button>
-          <Button component={Link} to="/login" variant="contained" color="primary">
+          </Button>}
+          {token ? "" : <Button component={Link} to="/login" variant="contained" color="primary">
           Login
-          </Button>
-          <Button component={Link} to="/user" variant="contained" color="primary">
-            Profile
-          </Button>
+          </Button>}
+          {token ? <Button component={Link} to="/user" variant="contained" color="primary">Profile</Button> : ""}
+
+          {token ? <Button onClick={handleLogOut} variant="contained" color="primary">Log out</Button> : ""}
         </Toolbar>
       </AppBar>
     </Box>
       <myContext.Provider value={{config, token}}>
       <Routes>
         <Route path="/register" element = { <Register />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/book" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<User />} />
       </Routes>
