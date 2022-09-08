a56e503b31b5c4795217aad36af6732da8470dfc
johnsjoden <johnsjoden95@gmail.com>
2022-09-08 14:30:41 +0200

    made error handling but need to fix backend for better message

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,16 @@
 import { Button } from '@mui/material'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import ErrorMessage from '../components/ErrorMessage'
 import MyInput from '../components/MyInput'
 
 export default function Register() {
     const navigate = useNavigate()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [message, setMessage] = useState(null)
+    const [usernameError, setUsernameError] = useState(null)
+    const [passwordError, setPasswordError] = useState(null)
     const registerUser = (e) => {
         e.preventDefault()
         const payload = {
@@ -18,16 +22,24 @@ export default function Register() {
             headers: {"content-type": "application/json"},
             body: JSON.stringify(payload)
         })
-        .then(res => {
-            navigate("/login")
+        .then(res => res.json())
+        .then(data => {
+            if(data.register === "failed"){
+                setPasswordError(data.username)
+                setUsernameError(data.password)
+            }else {
+                navigate("/login")
+            }
         })
     }
   return (
       <div>
         <h1>Register account</h1>
         <form onSubmit={registerUser}>
-            <MyInput value={username} setValue={setUsername} placeholder={"username"} /> 
+            <MyInput value={username} setValue={setUsername} placeholder={"username"} />
+            <ErrorMessage message={usernameError}/>
             <MyInput value={password} setValue={setPassword} placeholder={"password"} />
+            <ErrorMessage message={passwordError} />
             <Button type="submit">Submit</Button>
         </form>
         <Button component={Link} to="/login" color="primary">Already have a account? Login</Button>
