8eabf651d00d70e02b7a86ad2e28b2173184957d
johnsjoden <johnsjoden95@gmail.com>
2022-09-08 14:47:17 +0200

    erroro handling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,13 +2,14 @@ import {React, useState} from 'react'
 import MyInput from '../components/MyInput'
 import { Alert, Button } from '@mui/material'
 import {Link, useNavigate}  from "react-router-dom"
+import ErrorMessage from '../components/ErrorMessage'
 export default function Login() {
     const navigate = useNavigate()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [button, setButton] = useState(true)
+    const [usernameError, setUsernameError] = useState(null)
     const [type, setType] = useState("password")
-    const [error, setError] = useState(null)
     const handleOnClick = () => {
         if(button == true){
             setButton(false)
@@ -31,8 +32,9 @@ export default function Login() {
         }, )
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             if(data.user.token === "no user found"){
-                setError(data.user.token)
+                setUsernameError(data.user.token)
             }else {
                 const token = data.user.token
                 localStorage.setItem("key", token)
@@ -47,11 +49,11 @@ export default function Login() {
         <h1>Login</h1>
         <form onSubmit={handleOnSubmit}>
             <MyInput value={username} setValue={setUsername} placeholder={"username"} type={"text"} />
+            <ErrorMessage message={usernameError}/>
             <MyInput value={password} setValue={setPassword} placeholder={"password"} type={type}/>
             <Button onClick={handleOnClick}>{button ? "show password": "hide password"}</Button>
             <Button type="submit">Login</Button>
         </form>
-        {error? <Alert severity='error'>{error}</Alert>: "" }
         <Button component={Link} to="/register" color="primary">Register Account</Button>
      </div>
   )
