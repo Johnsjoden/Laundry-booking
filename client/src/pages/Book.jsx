94f17640b988026a8a26126a267115ad6e6d4dee
john sjödén <johnsjoden95@gmail.com>
2022-09-08 09:28:22 +0200

    made so you can choice different weeks

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -7,13 +7,14 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { Alert, Grid, Paper } from '@mui/material'
+import { Alert, CircularProgress, Grid, Paper } from '@mui/material'
 import { Container } from '@mui/system'
 export default function Home() {
   let [date, setDate] = useState(new Date())
   const [result, setResult] = useState([])
   const [open, setOpen] = React.useState(false);
   const [laundryData, setLaundryData] = useState({})
+  const [week, setWeek] = useState(0)
   const {
     token,
     config
@@ -36,13 +37,13 @@ export default function Home() {
     p: 4,
   };
   const fetchData = () => {
-    fetch(`http://localhost:5000/laundry/create`, config)
+    fetch(`http://localhost:5000/laundry/create?week=${week}`, config)
     .then(res => res.json())
     .then(data => setResult(data.result))
   }
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [week])
   const bookLaundry = () => {
     fetch(`http://localhost:5000/laundry`, {
             method: "POST",
@@ -56,15 +57,30 @@ export default function Home() {
         })
         // do a allert <Alert severity="success">This is a success alert — check it out!</Alert>
   }
+  const nextWeek = () => {
+    setWeek(week + 7)
+  }
+  const previousWeek = () => {
+    setWeek(week - 7)
+  }
+  console.log(result)
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    <Container>
+      <Box sx={{ display: "flex"}}>
+      <Button onClick={e => previousWeek()}>previous week</Button>
+        <Button onClick={e => nextWeek()}>next week</Button>
+      </Box>
+      <Grid sx={{ display: "flex"}} >
         {result.map((item, index) => {
-        return <Grid item xs={1} sm={1.7} md={1.7} key={index} sx={{border: 1, borderColor: "primary.main"}}>
+        return <Grid item xs={1} sm={1.7} md={1.7} key={index} sx={{ borderColor: "primary.main", p: "20px", m: "10px", boxShadow: "3"}}>
           <p>{item.date}</p>
         <span>{item.day} </span>
+        {item.bookingTimes.map((item, index) => {
+         return <div key={index}>
+            <Button disabled={item.booked || item.notActive} onClick={e => handleOpen(item)} >{item.timeStarted}</Button>
+          </div>
+        })}
         <p>{item.timeStarted}-{item.timeEnd}</p>
-        <Button disabled={item.booked || item.notActive} onClick={e => handleOpen(item)} >book</Button>
       </Grid>
       
       })}
@@ -76,7 +92,7 @@ export default function Home() {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            {laundryData.date} {laundryData.day} {laundryData.timeEnd}-{laundryData.timeStarted}
+            {laundryData.date} {laundryData.day} {laundryData.timeStarted}-{laundryData.timeEnd}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             <Button onClick={e => bookLaundry(laundryData.id)}>book</Button>
@@ -84,7 +100,8 @@ export default function Home() {
         </Box>
       </Modal>
       </Grid>
-      </Box>
 
+    </Container>
+    
   )
 }
