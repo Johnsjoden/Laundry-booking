5cd02d9af28eaec4e2ed632f1056f7b423e17a1e
john sjödén <johnsjoden95@gmail.com>
2022-09-11 15:30:24 +0200

    removed comments

--- server/index.js
+++ server/index.js
@@ -9,48 +9,6 @@ require("dotenv").config()
 app.use(cors())
 mongoose.connect("mongodb://localhost:27017/laundry")
 const PORT = process.env.PORT
-const getDay = (day) => {
-    console.log(day)
-    if(day === 1 || day === 8){
-        console.log(day)
-        return "Monday"
-    } else if (day === 2 || day === 9){
-        return "Tuesday"
-    }
-    else if (day === 3 || day === 10){
-        return "Wednesday"
-    }
-    else if (day === 4 || day === 11){
-        return "Thursday"
-    }
-    else if (day === 5 || day === 12){
-        return "Friday"
-    }
-    else if (day === 6 || day === 13){
-        return "Saturday"
-    }else if (day === 7 || day === 14){
-        return "Sunday"
-    }
-}
-/* ,
-    [{
-        day: getDay(new Date().getDay()),
-        timestarted: "16:00",
-        timeEnd: "19:00",
-        booked: false
-    }],
-    [{
-        day: getDay(new Date().getDay()),
-        timestarted: "13:00",
-        timeEnd: "16:00",
-        booked: false
-    }],
-    [{
-        day: getDay(new Date().getDay()),
-        timestarted: "10:00",
-        timeEnd: "13:00",
-        booked: false
-    }]) */
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(auth)
